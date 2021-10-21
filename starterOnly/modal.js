7bf1d9712dc59be373d8e69c799662adb11c6035
jimmy lelievre <jimmylelievre@iMac-de-jimmy.local>
2021-10-21 21:55:43 +0200

    Ajout des messages d'erreur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,7 +13,19 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeForm = document.querySelector(".close");
 const inputName = document.querySelector("[name='first']");
-
+const inputLastName = document.querySelector("[name='last']");
+const form = document.querySelector("[name='reserve']");
+const inputQuantity = document.querySelector("[name='quantity']");
+const inputBirth = document.querySelector("[name='birthdate']");
+const inputEmail = document.querySelector("[name='email']");
+const inputCheckbox = document.querySelector("input[value='checked']");
+const errorName = document.querySelector(".error-name");
+const errorLast = document.querySelector(".error-last");
+const errorEmail = document.querySelector(".error-email");
+const errorQuantity = document.querySelector(".error-quantity");
+const errorBirth = document.querySelector(".error-birthdate");
+const errorCheckbox = document.querySelector(".error-checkbox");
+console.log(inputCheckbox);
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -28,12 +40,98 @@ function launchModal() {
 }
 
 // Verifier le prénom
+let prenom = false;
 inputName.addEventListener("input", (e) => {
   if (e.target.value.length >= 2) {
-    valid = true;
+    prenom = true;
     console.log("ok !");
+    errorName.style.display = "none";
   } else {
     console.log("pas ok");
-    valid = false;
+    prenom = false;
+    errorName.style.display = "block";
+  }
+});
+
+// Vérifier le nom
+let lastName = false;
+inputLastName.addEventListener("input", (e) => {
+  if (e.target.value.length >= 2) {
+    lastName = true;
+    console.log("ok !");
+    errorLast.style.display = "none";
+  } else {
+    console.log("pas ok");
+    lastName = false;
+    errorLast.style.display = "block";
+  }
+});
+
+// Vérifier l'email
+let email = false;
+inputEmail.addEventListener("input", (e) => {
+  if (e.target.value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+    email = true;
+    console.log("ok !");
+    errorEmail.style.display = "none";
+  } else {
+    console.log("pas ok");
+    email = false;
+    errorEmail.style.display = "block";
+  }
+});
+
+// Vérifier l'anniversaire
+let birth = false;
+inputBirth.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  if (e.target.value == "") {
+    birth = false;
+    errorBirth.style.display = "block";
+    console.log("pas ok !");
+  } else {
+    console.log(" ok");
+    birth = true;
+    errorBirth.style.display = "none";
+  }
+});
+
+// Vérifier le nombre concours
+let quantity = false;
+inputQuantity.addEventListener("input", (e) => {
+  if (e.target.value > 0) {
+    quantity = true;
+    console.log("ok !");
+    errorQuantity.style.display = "none";
+  } else {
+    console.log("pas ok");
+    quantity = false;
+    errorQuantity.style.display = "block";
+  }
+});
+
+// Vérifier la checkbox
+let check = false;
+inputCheckbox.addEventListener("click", () => {
+  if (inputCheckbox.checked) {
+    console.log("ok");
+    check = true;
+    errorCheckbox.style.display = "none";
+  } else {
+    console.log("pas ok");
+    check = false;
+    errorCheckbox.style.display = "block";
+  }
+});
+
+// Validation du formulaire
+function validate() {}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (prenom && lastName && quantity && birth) {
+    alert("C'est ok !");
+  } else {
+    alert("remplir tous les champs !");
   }
 });

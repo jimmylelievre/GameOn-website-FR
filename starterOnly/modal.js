d14f8bc089f5485cb98ff4a707fa71125e725f39
jimmy lelievre <jimmylelievre@iMac-de-jimmy.local>
2021-10-21 12:52:50 +0200

    Mise en place de la fermeture du formulaire et de la condition pour l'input prenom

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,29 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeForm = document.querySelector(".close");
+const inputName = document.querySelector("[name='first']");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close the form
+closeForm.addEventListener("click", () => {
+  modalbg.style.display = "none";
+});
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Verifier le prénom
+inputName.addEventListener("input", (e) => {
+  if (e.target.value.length >= 2) {
+    valid = true;
+    console.log("ok !");
+  } else {
+    console.log("pas ok");
+    valid = false;
+  }
+});
